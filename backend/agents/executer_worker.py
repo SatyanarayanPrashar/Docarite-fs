196bf36df175905cbc378f1b3df6928c4f80a95d
Satyanarayan Prashar <satyanarayanprashar73@gmail.com>
2025-06-13 22:57:49 +0530

    prep new backend

--- backend/agents/executer_worker.py
+++
@@ -1,5 +0,0 @@
-from termcolor import colored
-
-def executor_worker(message: str) -> str:
-    print(colored(f"Processing message: {message}", "red"))
-    return "Documentation is created successfully."
--- backend/agents/readme_worker/readme_tool.py
+++
@@ -1,7 +0,0 @@
-from langchain_core.tools import Tool
-from tools.create_tools import read_file_content, write_file_content
-
-readme_tools = [
-    Tool.from_function(read_file_content, name="read_file_content", description="Read the content of a specific file within the cloned repository. Provide the full path relative to the base directory (e.g., 'clone_repos/repo_name/file.py')."),
-    Tool.from_function(write_file_content, name="write_file_content", description="Write content to a specific file within the cloned repository (e.g., 'clone_repos/repo_name/README.md'). This will overwrite existing files or create new ones."),
-]
